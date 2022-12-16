125c3b85e5afd37a82f4db55b1f7efd5a1e3956a
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-16 21:12:01 +0300

    task with names edit

--- 16.12 Функции/class/script/script_func.js
+++ 16.12 Функции/class/script/script_func.js
@@ -5,11 +5,19 @@
 // showMassage();
 
 
-function sayHi(name,name2,name3){
-    alert("Hi "+name)
-    alert("bye "+ name2);
-    alert("good day "+ name3)
+// function sayHi(name,name2,name3){
+//     alert("Hi "+name)
+//     alert("bye "+ name2);
+//     alert("good day "+ name3)
+// }
+
+
+// sayHi("Vasya","Petya","Masha");
+
+function sayHi(gd,name){
+    alert(gd + name);
 }
 
-
-sayHi("Vasya","Petya","Masha");
+sayHi("Hi ","Vasya");
+sayHi("bye ","Petya");
+sayHi("good day ","Masha");
