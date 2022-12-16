eb82957031f7582445a1c05e36d95757dd39900e
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-16 20:13:04 +0300

    object Math

---
+++ 16.12 Функции/class/script/script.js
@@ -0,0 +1,23 @@
+// объект мат вычислений Math
+ 
+let x = 5;
+let n = 3;
+
+let y = (2*Math.exp(x))+1;
+
+
+console.log(Math.ceil(y));
+
+
+let z = Math.log(x)/x;
+
+console.log(z);
+
+let w ;
+
+if (x < Math.pow(z,2)){
+    console.log(w =x*y);
+}else if(x >= Math.pow(z,2)){
+    console.log( w = n*x +2);
+}
+
