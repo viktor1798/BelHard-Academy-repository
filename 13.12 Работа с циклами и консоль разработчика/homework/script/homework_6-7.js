c21093ece12097ae0d1be1562245e679026f9937
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-16 19:38:19 +0300

    homework solition task 7

--- 13.12 Работа с циклами и консоль разработчика/homework/script/homework_6-7.js
+++ 13.12 Работа с циклами и консоль разработчика/homework/script/homework_6-7.js
@@ -1,9 +1,37 @@
 //6
-for(let i=1; i<=11; i++ ){
-    if(i%2==0){
-        // idword4.innerHTML += i + " ";
-        boximg6.innerHTML += `<img src="images/${i}.jpg" style="border:2px solid green"; alt="" >`
-    }else boximg6.innerHTML += `<img src="images/${i}.jpg" style="border: 2px solid blue"; alt="" >`
-}
+// for(let i=1; i<=11; i++ ){
+//     if(i%2==0){
+//         // idword4.innerHTML += i + " ";
+//         boximg6.innerHTML += `<img src="images/${i}.jpg" style="border:2px solid green"; alt="" >`
+//     }else boximg6.innerHTML += `<img src="images/${i}.jpg" style="border: 2px solid blue"; alt="" >`
+// }
 
 //7
+
+let columCount = +prompt("Количество столбцов?");
+let rowCount = +prompt("Введите строки");
+
+let count = 1;
+
+let myTable = "";
+
+
+
+for(let i=1; i<=rowCount; i++){
+    myTable += "<tr>"
+        for(let j = 1; j<= columCount; j++){
+            myTable += `<td> Ячейка ${count++} </td>`            
+        }
+    myTable +="</tr>"
+}
+
+myTable = `<table> 
+                <tbody>
+                    ${myTable} 
+                </table>
+            </tbody>`;
+
+
+console.log(myTable);
+
+table.innerHTML = myTable;
