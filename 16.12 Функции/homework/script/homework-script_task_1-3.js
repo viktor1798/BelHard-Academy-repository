9c4762b7ab54f9f5c584350569fe980cbb0c2ce5
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-19 18:53:35 +0300

    task 3.2 solitions

--- 16.12 Функции/homework/script/homework-script_task_1-3.js
+++ 16.12 Функции/homework/script/homework-script_task_1-3.js
@@ -6,29 +6,60 @@
 //     result = 'Много';
 //   }
 
-let num1 = +prompt("num1");
-let num2 = +prompt("num2");
+// let num1 = +prompt("num1");
+// let num2 = +prompt("num2");
 
-function thenarOperator(a,b){
-    let operator = (a + b < 4) ?alert ("Мало"):alert("Много");
-    return operator;
-}
+// function thenarOperator(a,b){
+//     let operator = (a + b < 4) ?alert ("Мало"):alert("Много");
+//     return operator;
+// }
 
-function switchOperator (a,b){
-    let x = a+b;
+// function switchOperator (a,b){
+//     let x = a+b;
     
-    switch(x<4){
-        case true:
-            return alert("Мало");
+//     switch(x<4){
+//         case true:
+//             return alert("Мало");
 
-        case false:
-            return alert ("Много");
+//         case false:
+//             return alert ("Много");
         
-        default:
-            return alert("gg");
-    }
-}
+//         default:
+//             return alert("gg");
+//     }
+// }
 
 
 // thenarOperator(num1,num2);
-switchOperator(num1,num2);
+//switchOperator(num1,num2);
+
+//2
+
+
+
+
+
+
+
+
+
+
+
+
+//3 
+
+function equationTwo(x,y){
+    let a = Math.pow(Math.cos((4*x)),2)- (1+Math.cos(4*y)/2);
+    let r;
+    if(a < Math.sin(Math.pow(y,2))){
+        r = Math.PI/4-x;
+    } else if(a>=Math.pow(Math.sin(y-1),2)){
+        r = Math.tan(x)+tan(Math.PI/4*x)
+    }
+
+    return alert(r);
+}
+
+
+
+equationTwo(0.8,-3.75)
