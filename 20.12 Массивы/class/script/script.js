bebc2f6cc47374e50d690cfeb959f6b256fb5c36
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-20 19:45:13 +0300

    teory array

--- 20.12 Массивы/class/script/script.js
+++ 20.12 Массивы/class/script/script.js
@@ -1,15 +1,50 @@
-// function decloration
+// // function decloration
 
-function nameFunc(a,b){
-    return alert(a+b);
+// function nameFunc(a,b){
+//     return alert(a+b);
+// }
+
+// nameFunc(2,3)
+
+// //function exprassion
+
+// let nameFunc2=(z,x)=>{
+//     return alert(z+x)
+// }
+
+// nameFunc2(4,3);
+
+//Array
+
+//конструктор
+
+let arr1=new Array(); //пустой массив
+let arr2=new Array(5); //массив с 5-ю элементами
+let arr3=new Array("One", "Two", "three"); //массив с 3-я элементами типа String
+
+let arr4 = []; //пустой массив
+let arr5 = ["One", "Two", "three"]; //массив с 3-я элементами типа String
+
+// Вывод элементов
+
+console.log(arr5);
+console.log(arr5[1]);
+arr5[1]= "No Two"
+console.log(arr5[1]);
+console.log(arr5);
+arr5[3]= "four"
+console.log(arr5);
+// arr5[5]= "five"   // не имеет смысла
+// console.log(arr5);
+
+
+// перебор массива
+
+for(let i=0;  i<=3;i++){
+    console.log(arr5[i]);
 }
 
-nameFunc(2,3)
-
-//function exprassion
-
-let nameFunc2=(z,x)=>{
-    return alert(z+x)
+// for...of...
+for(let em of arr5){
+    console.log(em);
 }
-
-nameFunc2(4,3);
