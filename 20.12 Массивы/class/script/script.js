cc42302ba2815c2e820f1f5c097a7aea70307313
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-20 19:13:39 +0300

    repeat func

--- 20.12 Массивы/class/script/script.js
+++ 20.12 Массивы/class/script/script.js
@@ -1,37 +1,15 @@
-// объект мат вычислений Math
- 
-// let x = 5;
-// let n = 3;
+// function decloration
 
-// let y = (2*Math.exp(x))+1;
+function nameFunc(a,b){
+    return alert(a+b);
+}
 
+nameFunc(2,3)
 
-// console.log(Math.ceil(y));
+//function exprassion
 
+let nameFunc2=(z,x)=>{
+    return alert(z+x)
+}
 
-// let z = Math.log(x)/x;
-
-// console.log(z);
-
-// let w ;
-
-// if (x < Math.pow(z,2)){
-//     console.log(w =x*y);
-// }else if(x >= Math.pow(z,2)){
-//     console.log( w = n*x +2);
-// }
-
-
-let w,m,n,k,a,b;
-
-m = Math.sin(5);
-n = Math.exp(m);
-k = m+n;
-a = Math.pow(Math.log(n),2)+m;
-b = Math.acos(k)/a;
-
-if(a < Math.pow(b,3) ){
-    w = Math.sqrt(a+1);
-}else w = Math.sqrt(a+2);
-    
-console.log(w);
+nameFunc2(4,3);
