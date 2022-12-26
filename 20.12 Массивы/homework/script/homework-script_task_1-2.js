fa965e7e98ae87aa99dcc7d01bd423a8d2dd7d56
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-26 13:23:50 +0300

    task 2 homework 20.12 lesson

--- 20.12 Массивы/homework/script/homework-script_task_1-2.js
+++ 20.12 Массивы/homework/script/homework-script_task_1-2.js
@@ -1,26 +1,57 @@
-//1
+// //1
 
-function sumArray() {
+// function sumArray() {
 
-    let arrayNum =[];
+//     let arrayNum =[];
 
-    while (true){
-        let value = prompt("Введите число!:")
+//     while (true){
+//         let value = prompt("Введите число!:")
 
-        if(value === null || value === "" || !isFinite(value)){
-            break;
-        }
+//         if(value === null || value === "" || !isFinite(value)){
+//             break;
+//         }
 
-        arrayNum.push(+value);
-    }
-    let sum = 0;
+//         arrayNum.push(+value);
+//     }
+//     let sum = 0;
 
-    for(let number of arrayNum){
-        sum += number;
-    }
+//     for(let number of arrayNum){
+//         sum += number;
+//     }
 
-    return sum;
+//     return sum;
     
+// }
+
+// alert(sumArray());
+
+
+// 2
+//test
+// let d = [12, 0.4, 30, -2, 9.3]
+
+// let max = Math.max(...d);
+// let index = d.indexOf(max);
+// alert (index);
+// alert(max)
+
+function maxElArray(){
+    let d =[]
+    let max, index;
+    while (true){
+    let value = prompt("Введите число!:")
+    
+    if(value === null || value === "" || !isFinite(value)){
+        break;
+    }
+
+       d.push(+value);
+    }
+
+    max = Math.max(...d);
+    index = d.indexOf(max);
+
+    return `Число ${max} c индексом ${index}`    
 }
 
-alert(sumArray());
+alert(maxElArray());
