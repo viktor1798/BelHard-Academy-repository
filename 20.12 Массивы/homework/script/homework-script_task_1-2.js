dc75e9c87dab5f67fd219098a919bc21d8261e14
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-26 12:30:05 +0300

    task 1 homework 20.12 lesson

--- 20.12 Массивы/homework/script/homework-script_task_1-2.js
+++ 20.12 Массивы/homework/script/homework-script_task_1-2.js
@@ -1,2 +1,26 @@
-//3
+//1
 
+function sumArray() {
+
+    let arrayNum =[];
+
+    while (true){
+        let value = prompt("Введите число!:")
+
+        if(value === null || value === "" || !isFinite(value)){
+            break;
+        }
+
+        arrayNum.push(+value);
+    }
+    let sum = 0;
+
+    for(let number of arrayNum){
+        sum += number;
+    }
+
+    return sum;
+    
+}
+
+alert(sumArray());
