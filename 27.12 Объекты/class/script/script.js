3b9203242ed4a16eb6346340164ee052b82de5e2
viktor1798 <vktrsmsk1798@gmail.com>
2022-12-27 19:31:45 +0300

    teory object 2

--- 27.12 Объекты/class/script/script.js
+++ 27.12 Объекты/class/script/script.js
@@ -4,14 +4,45 @@
 
 //person={};  // пустой объект
 
-let person = {
-    firstName : "Sasha",
-    lastName: "Petrov",
-    id: 5566,
+// let person = {
+//     firstName : "Sasha",
+//     lastName: "Petrov",
+//     id: 5566,
 
-    fullInfo:()=>{
-        return alert(`name: ${person.firstName}, lastname: ${person.lastName}, id:${person.id}`) // метод объекта person
-    }
-}
+//     fullInfo:()=>{
+//         //this - ссылается на владельца метода, то есть родителя, то есть объект person
+//         return alert(`name: ${this.firstName}, lastname: ${this.lastName}, id:${this.id}`) // метод объекта person
+//     }
+// }
 
-person.fullInfo(); // обращение к свойствам объекта
+//person.fullInfo(); // обращение к методу
+
+
+
+//
+
+person={};
+
+person.age = 18;
+person.firstName = "Igor";
+console.log(person.firstName, person.age , person);
+delete person.age;
+
+//просмотро содержимого в Объекте person
+if ("firstName" in person){
+    console.log("существует");
+}else console.log("не существует");
+
+//доступ через квадратные скобки []
+
+person["firstName"]="Petya";
+
+console.log(person["firstName"])
+
+// доступ к свойству объекта через переменную
+
+person.age = 30;
+
+let key = "age";
+
+console.log(person[key]);
