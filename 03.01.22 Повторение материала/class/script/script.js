d5612ba1eddd42058539e24181799350e20de0a0
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-03 19:42:20 +0300

    array of objects with no element name

--- 03.01.22 Повторение материала/class/script/script.js
+++ 03.01.22 Повторение материала/class/script/script.js
@@ -46,30 +46,47 @@
 
 // объекты с разными именами внутри массива
 
+// let arrObj = [
+//     { pic1: { f:"4.jpg",
+//         w:"500",
+//         h: "500",
+//     },}, //0
+//     {pic2: { f:"5.jpg",
+//     w:"700",
+//     h: "700",
+//     },}, //1
+//     {pic3: { f:"6.jpg",
+//     w:"900",
+//     h: "900",
+//     },} //2
+// ]
+//     for(let i = 0 ; i<arrObj.length;i++){
+       
+//         for (const key in arrObj[i]) {
+//             let f,w,h
+//             f= arrObj[i][key].f;
+//             w = arrObj[i][key].w;
+//             h = arrObj[i][key].h;
+
+//             // console.log(f,w,h);
+//             box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
+//         }
+
+//     }
+
+//массив объектов без имени элементов
+
 let arrObj = [
-    { pic1: { f:"1.jpg",
-        w:"500",
-        h: "500",
-    },}, //0
-    {pic2: { f:"2.jpg",
-    w:"700",
-    h: "700",
-    },}, //1
-    {pic3: { f:"3.jpg",
-    w:"900",
-    h: "900",
-    },} //2
+    { f:"7.jpg",w:"500",h: "500",}, //0
+    { f:"8.jpg",w:"700",h: "700",}, //1
+     { f:"9.jpg",w:"900",h: "900",} //2
 ]
     for(let i = 0 ; i<arrObj.length;i++){
-       
-        for (const key in arrObj[i]) {
-            let f,w,h
-            f= arrObj[i][key].f;
-            w = arrObj[i][key].w;
-            h = arrObj[i][key].h;
+        let f,w,h
+        f= arrObj[i].f;
+        w = arrObj[i].w;
+        h = arrObj[i].h
 
-            // console.log(f,w,h);
-            box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
-        }
+        box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
 
     }
