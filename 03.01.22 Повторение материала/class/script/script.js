91ead5a803caf51b602ab26dc6965522d0b1bef1
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-03 19:36:34 +0300

    objects with different names inside an array

--- 03.01.22 Повторение материала/class/script/script.js
+++ 03.01.22 Повторение материала/class/script/script.js
@@ -20,27 +20,56 @@
 
 // массив из объектов
 
+// let arrObj = [
+//     { pic: { f:"1.jpg",
+//         w:"500",
+//         h: "500",
+//     },}, //0
+//     {pic: { f:"2.jpg",
+//     w:"700",
+//     h: "700",
+//     },}, //1
+//     {pic: { f:"3.jpg",
+//     w:"900",
+//     h: "900",
+//     },} //2
+// ]
+//     for(let i = 0 ; i<arrObj.length;i++){
+//         let f,w,h
+//         f= arrObj[i].pic.f;
+//         w = arrObj[i].pic.w;
+//         h = arrObj[i].pic.h
+
+//         box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
+
+//     }
+
+// объекты с разными именами внутри массива
+
 let arrObj = [
-    { pic: { f:"1.jpg",
+    { pic1: { f:"1.jpg",
         w:"500",
         h: "500",
     },}, //0
-    {pic: { f:"2.jpg",
+    {pic2: { f:"2.jpg",
     w:"700",
     h: "700",
     },}, //1
-    {pic: { f:"3.jpg",
+    {pic3: { f:"3.jpg",
     w:"900",
     h: "900",
     },} //2
 ]
     for(let i = 0 ; i<arrObj.length;i++){
-        let f,w,h
-        f= arrObj[i].pic.f;
-        w = arrObj[i].pic.w;
-        h = arrObj[i].pic.h
+       
+        for (const key in arrObj[i]) {
+            let f,w,h
+            f= arrObj[i][key].f;
+            w = arrObj[i][key].w;
+            h = arrObj[i][key].h;
 
-        box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
+            // console.log(f,w,h);
+            box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
+        }
 
     }
-
