96924fcc96451e85256f7317f62ad8180aefa5d6
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-03 19:26:41 +0300

    an array of objects. OOP

--- 03.01.22 Повторение материала/class/script/script.js
+++ 03.01.22 Повторение материала/class/script/script.js
@@ -1,19 +1,46 @@
 //многомерный массив
 
-let matrix =[
-[3,3,3], //0
-[2,4,3], //1
-[3,2,6], //2
-[0,2,6]  //3
-];
+// let matrix =[
+// [3,3,3], //0
+// [2,4,3], //1
+// [3,2,6], //2
+// [0,2,6]  //3
+// ];
 
-console.log(matrix[3][0]);
+// console.log(matrix[3][0]);
 
-//объект из массивов 
+// //объект из массивов 
 
-let obj = {
-    b:[[1,2],5],
-    c:[2,4,[6,3]],
-}
+// let obj = {
+//     b:[[1,2],5],
+//     c:[2,4,[6,3]],
+// }
 
-console.log(obj.b[0][0])
+// console.log(obj.b[0][0]);
+
+// массив из объектов
+
+let arrObj = [
+    { pic: { f:"1.jpg",
+        w:"500",
+        h: "500",
+    },}, //0
+    {pic: { f:"2.jpg",
+    w:"700",
+    h: "700",
+    },}, //1
+    {pic: { f:"3.jpg",
+    w:"900",
+    h: "900",
+    },} //2
+]
+    for(let i = 0 ; i<arrObj.length;i++){
+        let f,w,h
+        f= arrObj[i].pic.f;
+        w = arrObj[i].pic.w;
+        h = arrObj[i].pic.h
+
+        box.innerHTML += `<img src="images/${f}" alt="" style="width:${w}px; height:${h}px"></img>`
+
+    }
+
