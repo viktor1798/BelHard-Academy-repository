c655011bd16654923770775d5fe7674fb6ce4bfa
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-10 19:44:00 +0300

    getelementsbyclassname

--- 10.01.23 Поиск элементов в/class/script/script.js
+++ 10.01.23 Поиск элементов в/class/script/script.js
@@ -18,30 +18,44 @@
 
 
 
-function change2(){
-    //links стал массивом элементов а
+// function change2(){
+//     //links стал массивом элементов а
     
-    // let links = document.getElementsByTagName("a");
-    // for (const index of links) {
-    //     index.innerHTML = "edit"
-    //     index.style.color = "red"
-    // }
-    let  sctn = document.getElementById("findHere")
+//     // let links = document.getElementsByTagName("a");
+//     // for (const index of links) {
+//     //     index.innerHTML = "edit"
+//     //     index.style.color = "red"
+//     // }
+//     let  sctn = document.getElementById("findHere")
    
-    //     let links = sctn .getElementsByTagName("a");
-    // for (const index of links) {
-    //     index.innerHTML = "edit"
-    //     index.style.color = "red"
-    // }
+//     //     let links = sctn .getElementsByTagName("a");
+//     // for (const index of links) {
+//     //     index.innerHTML = "edit"
+//     //     index.style.color = "red"
+//     // }
 
 
-    let link = sctn .getElementsByTagName("a");
+//     let link = sctn .getElementsByTagName("a");
 
-    for (let i = 0 ; i <= link.length; i++){
-        link[i].innerHTML = `Link ${i+1}`;
+//     for (let i = 0 ; i <= link.length; i++){
+//         link[i].innerHTML = `Link ${i+1}`;
 
-        link[i].href = `link${i+1}.html`
+//         link[i].href = `link${i+1}.html`
 
+//     }
+
+// }
+
+
+//getElementsByClassName
+
+
+
+function change3(){
+    let el = document.getElementsByClassName("myBorder");
+    console.log(el)
+    for (const index of el) {
+        index.innerHTML = "edit"
+        index.style.border= "2px solid black"
     }
-
 }
