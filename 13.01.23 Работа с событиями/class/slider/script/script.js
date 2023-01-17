c5bfb6e7a2019fd112d116e8def03380e7fe18e4
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-17 20:49:40 +0300

    solition homework task. learn object window

--- 13.01.23 Работа с событиями/class/slider/script/script.js
+++ 13.01.23 Работа с событиями/class/slider/script/script.js
@@ -1,16 +1,51 @@
 let i=1, count =6
-let arrHref = ["https://www.google.com/", "https://yandex.by","https://translate.google.by/","https://leetcode.com/","https://www.efset.org/ru/","https://www.facebook.com/"]
+let arrHref = ["https://www.google.com/", "https://yandex.by/","https://translate.google.by/","https://leetcode.com/","https://www.efset.org/ru/","https://www.facebook.com/"]
 
 function stop(){
     (clearTimeout(stopTime))
 }
 
-function imgS() {
-    if (i > count) {
-        i = 1
+// отдельая функция, ибо идет повторение кода
+
+function linkText(){
+    for(let index = 0; index<arrHref.length;i++){
+        console.log(arrHref[index])
     }
+}
+linkText()
+function changeSlider(){
     link.href = arrHref[i-1]
     slider.src = `images/img${i++}b.jpg`;
 
+    title1.innerHTML =`Заголовок слайда ${i}`
+    title1text.innerHTML = `<a href="link${i}.html">Подзаголовок слайда ${i}</a>`
+    for(let index=0; index<arrHref.length;index++){
+        refsite.innerHTML=`<a href="${arrHref[index]}">${arrHref[index]}</a>`
+    }
+}
+
+function imgS() {
+    if (i > count || i==0) {
+        i = 1
+    }
+    changeSlider()
+
     stopTime = setTimeout(imgS, 2000);
+}
+
+function next(){
+    if (i > count) {
+        i = 1
+    }
+    changeSlider()
+
+}
+
+function before(){
+    if(i < 1){
+        i=6
+    }
+    link.href = arrHref[i-1]
+    slider.src = `images/img${i--}b.jpg`;
+
 }
---
+++ 17.01.23 Методы Window/class/script/script.js
@@ -0,0 +1,23 @@
+// методы для строк
+
+// let s = "https://www.google.com/";
+
+// let l = s.length;
+// console.log(l);
+
+// let pos = s.indexOf("//",0);
+
+// let str = s.substring(pos+2,l-1)
+// console.log(pos);
+// console.log(str);
+
+
+//объекты браузера window
+
+// document
+// navigator
+// history
+// screen
+// location
+
+// alert - является методом window;
